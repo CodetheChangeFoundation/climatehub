2a589d9b93500faad24a38784352cd1f72450213
vtotient <quique.sira@gmail.com>
2018-10-11 18:15:58 -0700

    CH-1: finished cf?

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php get_header(); ?>
   <h1>This is my index.</h1>
 <?php get_footer(); ?>
+
+<?php if( get_field('page_title') && get_field('paragraph')): ?>
+  <h2><?php the_field('page_title'); ?><h2>
+    <p><?php the_field('paragraph'); ?><\p>
+<?php endif; ?>
