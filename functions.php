756b71bf217cd593b542cfcf2997f76766af2f7a
Viniel Kumar <vinielk@icloud.com>
2018-10-13 19:40:00 -0700

    CH-8: added bootstrap 4 css and js

--- functions.php
+++ functions.php
@@ -1,11 +1,10 @@
 <?php
+/**
+ * Climate Hub Theme Functions
+ *
+ * @package climatehub
+ */
 
-function climatehub_script_enqueue() {
-
-  wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/climatehub.css', array(), '1.0.0', 'all');
-  wp_enqueue_script('customjs', get_template_directory_uri() . '/js/climatehub.js', array(), '1.0.0', true);
-
-}
-
-add_action('wp_enqueue_scripts', 'climatehub_script_enqueue');
-?>
+require get_template_directory() . '/inc/cleanup.php';
+require get_template_directory() . '/inc/enqueue.php';
+require get_template_directory() . '/inc/theme-support.php';
