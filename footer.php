f2591e03db7b631a00d425fd7f814a186306d60d
Melissa B <melissabernstein.ubc@gmail.com>
2018-10-17 17:55:32 -0700

    first draft

--- footer.php
+++ footer.php
@@ -3,13 +3,37 @@
   </footer>
 
 <link rel="stylesheet" type = "text/css" href="/assets/css/climatehub">
+<div class=".greenbox"> </div>
 
+<footer>
 <?php wp_footer(); ?>
-<div class=".greenbox">
-  <p>Sally</p>
-  </div>
+
+    <style>
+      footer {
+        background-color: #DFEBD3;
+      }
+      copyright {
+        text-align: center;
+        background-size: cover;
+      }
+      h1, h2, copyright {
+        display: inline-block;
+        background: #fff;
+      }
+      h1 {
+        font-size: 30px
+      }
+      h2 {
+        font-size: 20px;
+      }
+    </style>
+  <copyright>Copyright </copyright>
+  <h1>CopyRight message</h1>
+  <h2>Code the Change link</h2>
+    </footer>
 
    
 
+/*KEEP*/
   </body>
 </html>
