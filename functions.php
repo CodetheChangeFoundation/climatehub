431da66da3312ed249431586065c6686ee52ca06
vtotient <quique.sira@gmail.com>
2018-10-19 22:33:13 -0700

    CH-4: Forgot to include the get_template_directory

--- functions.php
+++ functions.php
@@ -8,4 +8,4 @@
 require get_template_directory() . '/inc/cleanup.php';
 require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/theme-support.php';
-// require get_template_directory() . '/inc/paragraph-cf.php';
+require get_template_directory() . '/inc/paragraph-cf.php';
