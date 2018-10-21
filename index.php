a5df195b5202368f723a60062bdff0cc5a732d7d
vtotient <quique.sira@gmail.com>
2018-10-20 21:00:51 -0700

    CH-4: Removed comment from title-cf.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-<div class="container">
-  <?php get_template_part( "template-parts/title" ); ?>
-</div>
-
 <?php get_header(); ?>
   <h1>This is my index.</h1>
+  
+  <div class="container">
+    <?php get_template_part( "template-parts/title" ); ?>
+  </div>
+
 <?php get_footer(); ?>
