08e62eab0c260ef68f294c997a020efb94cd006c
vtotient <quique.sira@gmail.com>
2018-10-20 20:57:11 -0700

    CH-4: Added requested changes from last PR

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
+<div class="container">
+  <?php get_template_part( "template-parts/title" ); ?>
+</div>
+
 <?php get_header(); ?>
   <h1>This is my index.</h1>
 <?php get_footer(); ?>
-
-<div class='container'>
-  <?php get_template_part( "template-parts/title" ); ?>
-</div>
