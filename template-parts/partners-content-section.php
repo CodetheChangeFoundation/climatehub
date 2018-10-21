dd924130717fe22e265eefffd7035a5646f6d4c2
Pritpal Chauhan <pchauhan@Pritpals-MacBook-Pro.local>
2018-10-20 17:16:59 -0700

    CH-9: Changed bootstrap classes for partners cf

--- template-parts/partners-content-section.php
+++ template-parts/partners-content-section.php
@@ -6,28 +6,28 @@
  */
 
 if( get_field('enable_partners_content_section') ): ?>
-<div class="container">
-  <section>
-    <div class="row m-3 w-100">
-      <div class="col-sm-12 col-md-8 col-lg-8 p-3">
-        <?php if( get_field('left_column_title') ): ?>
-        <h3><?php the_field('left_column_title')?></h3>
-        <?php endif ?>
+  <div class="container">
+    <section>
+      <div class="row m-3 w-100">
+        <div class="col-12 col-md-8 p-3">
+          <?php if( get_field('left_column_title') ): ?>
+            <h3><?php the_field('left_column_title')?></h3>
+          <?php endif ?>
 
-        <?php if( get_field('left_column_paragraph') ): ?>
-        <p><?php the_field('left_column_paragraph')?></p>
-        <?php endif ?>
-      </div>
-      <div class="col-sm-12 col-md-4 col-lg-4 p-3">
-        <?php if( get_field('right_column_title') ): ?>
-        <h3><?php the_field('right_column_title')?></h3>
-        <?php endif ?>
+          <?php if( get_field('left_column_paragraph') ): ?>
+            <p><?php the_field('left_column_paragraph')?></p>
+          <?php endif ?>
+        </div>
+        <div class="col-12 col-md-4 p-3">
+          <?php if( get_field('right_column_title') ): ?>
+            <h3><?php the_field('right_column_title')?></h3>
+          <?php endif ?>
 
-        <?php if( get_field('right_column_paragraph') ): ?>
-        <p><?php the_field('right_column_paragraph')?></p>
-        <?php endif ?>
+          <?php if( get_field('right_column_paragraph') ): ?>
+            <p><?php the_field('right_column_paragraph')?></p>
+          <?php endif ?>
+        </div>
       </div>
-    </div>
-  </section>
-</div>
+    </section>
+  </div>
 <?php endif ?>
