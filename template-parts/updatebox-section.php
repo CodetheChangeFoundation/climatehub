fd97752a577338453e7b28688e027e395991fb95
tiffanywu <hsinyunwu0313@gmail.com>
2018-10-24 18:26:00 -0700

    CH-10: update

--- template-parts/updatebox-section.php
+++ template-parts/updatebox-section.php
@@ -6,10 +6,24 @@
 
 
   if(get_field('enable_custom_field')): ?>
-  <div class="container">
+    <div class="container">
       <section>
-          <div class="row m-3 w-100">
-              <div class="col-12 col-md-8 p-3">
+        <div class="row">
+          <div class="col-sm">
+                  <?php if(get_field('title')): ?>
+                   <h3><?php echo get_field('title') ?></h3>
+                  <?php endif; ?>
+                  
+                  <?php if( get_field('project_description') ): ?>
+                   <p><?php echo get_field('project_description')?></p>
+                  <?php endif; ?>
+                  
+                  <?php if( get_field('project_image') ): ?>
+                   <img src="<?php the_field('project_image'); ?>" />
+                  <?php endif; ?>   
+            </div>
+          
+            <div class="col-sm">
                   <?php if(get_field('title')): ?>
                    <h3><?php echo get_field('title') ?></h3>
                   <?php endif; ?>
@@ -21,10 +35,23 @@
                   <?php if( get_field('project_image') ): ?>
                    <img src="<?php the_field('project_image'); ?>" />
                   <?php endif; ?>
+             </div>  
+            
+             <div class="col-sm">
+                  <?php if(get_field('title')): ?>
+                   <h3><?php echo get_field('title') ?></h3>
+                  <?php endif; ?>
                   
-              </div>
+                  <?php if( get_field('project_description') ): ?>
+                   <p><?php echo get_field('project_description')?></p>
+                  <?php endif; ?>
+                  
+                  <?php if( get_field('project_image') ): ?>
+                   <img src="<?php the_field('project_image'); ?>" />
+                  <?php endif; ?>
+             </div> 
           </div>
        </section>
-  </div>                 
-<?php endif; ?>
+     </div>
 
+<?php endif; ?>
