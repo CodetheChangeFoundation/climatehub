bc0a2c041cdd8047d384539bcbb61c6b00a578ee
Melissa B <melissabernstein.ubc@gmail.com>
2018-10-24 19:28:57 -0700

    copyright done

--- footer.php
+++ footer.php
@@ -1,31 +1,36 @@
 <footer>
-  
-<div class="container">
-  <div class="row">
-    <div class="col">
-      Link "Climate hub logo final.png"
+
+
+	<div class="container-fluid">
+  <div class="row bg-secondary" style="footer">
+    <div class="col-sm-2">
+      <img src="<?php echo get_template_directory_uri() . '/assets/images/ClimateHubLogo.png'; ?>" class="img-thumbnail" alt="ClimateHubLogo"/>
     </div>
-    <div class="col">
-      Add custom field here (Creating a vibrant community around bold climate action at UBC.)
+    <div class="col-sm-2">
+      Creating a vibrant community around bold climate action at UBC.
     </div>
-    <div class="col sm-12 text-center">
+    <div class="col-sm-4 text-center">
       <h6 class="font-weight-bold">FOLLOW US</h6>
-      [insert social icons]
+      <a href="https://wordpress.org/plugins/social-icons-widget-by-wpzoom/"> add social icons plugin</a>
     </div>
-    <div class="col">
-      Add custom field: donate words (Help us achieve our vision! We appreciate your support.)
+    <div class="col-sm-2 right">
+      Help us achieve our vision! We appreciate your support.
     </div>
-    <div class="col">
-      donate button
-    </div>
-  </div>
-  
-  <div class="row">
-    <div class="col-12 text-center">
-      <a href="http://codethechange.ca/"> UBC Code The Change</a> 
+    <div class="col-sm-2 align-middle">
+      <button type="button" class="btn btn-outline-primary font-italic align-middle">donate</button>
     </div>
   </div>
-</div>
+
+  <div class="row bg-dark text-white text-center">
+    <div class="col text-center" >
+        Copyright &copy; <script type="text/javascript">
+          document.write(new Date().getFullYear());
+        </script>
+         Climate Hub. Created by
+      <a href="http://codethechange.ca/"> Code the Change Foundation</a>
+
+    </div>
+  </div>
 
   </footer>
 
--- functions.php
+++ functions.php
@@ -10,7 +10,6 @@ require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/theme-support.php';
 
 /*require get_templaet_directory() . 'footer_customfield.php';*/
-=======
+
 
 require get_template_directory() . '/custom-fields/partners-content-section.php';
->>>>>>> e823f01d407ed00d34621430b78f87308e9dd53e
