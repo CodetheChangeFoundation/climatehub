cc0892611ac359def3aaa9ff8e830c1ba0f76b0d
tiffanywu <hsinyunwu0313@gmail.com>
2018-10-26 20:48:33 -0700

    updated changes

--- template-parts/updatebox-section.php
+++ template-parts/updatebox-section.php
@@ -4,57 +4,50 @@
  * @package climatehub
  */
 
-
-  if(get_field('enable_custom_field')): ?>
-    <div class="container">
-      <section>
-        <div class="row">
-          <div class="col-sm">
-                  <?php if(get_field('title')): ?>
-                   <h2><?php echo get_field('title') ?></h2>
-                  <?php endif; ?>
+if(get_field('update_box_enable_custom_field')): ?>
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-md-4">
+            <?php if(get_field('update_box_title')): ?>
+            <h2><?php echo get_field('update_box_title') ?></h2>
+            <?php endif; ?>
+            
+            <?php if( get_field('update_box_description') ): ?>
+            <p><?php echo get_field('update_box_description')?></p>
+            <?php endif; ?>
                   
-                  <?php if( get_field('project_description') ): ?>
-                   <p><?php echo get_field('project_description')?></p>
-                  <?php endif; ?>
-                  
-                  <?php if( get_field('project_image') ): ?>
-                   <img src="<?php echo get_field('project_image')['url']; ?>" class="img-fluid"/>
-                  <?php endif; ?>   
-            </div>
+            <?php if( get_field('update_box_image') ): ?>
+            <img src="<?php echo get_field('update_box_image')['url']; ?>" class="img-fluid"/>
+            <?php endif; ?>
+        </div>
         
-            
-            <div class="col-sm">
-                  <?php if(get_field('title')): ?>
-                   <h2><?php echo get_field('title') ?></h2>
-                  <?php endif; ?>
+        <div class="col-12 col-md-4">
+            <?php if(get_field('update_box_title')): ?>
+            <h2><?php echo get_field('update_box_title') ?></h2>
+            <?php endif; ?>
                   
-                  <?php if( get_field('project_description') ): ?>
-                   <p><?php echo get_field('project_description')?></p>
-                  <?php endif; ?>
+            <?php if( get_field('update_box_description') ): ?>
+            <p><?php echo get_field('update_box_description')?></p>
+            <?php endif; ?>
                   
-                  <?php if( get_field('project_image') ): ?>
-                    <img src="<?php echo get_field('project_image')['url']; ?>" class="img-fluid"/>
-                  <?php endif; ?>
-             </div>  
+            <?php if( get_field('update_box_image') ): ?>
+            <img src="<?php echo get_field('update_box_image')['url']; ?>" class="img-fluid"/>
+            <?php endif; ?>
+        </div>  
+        
+        <div class="col-12 col-md-4">
+            <?php if(get_field('update_box_title')): ?>
+            <h2><?php echo get_field('update_box_title') ?></h2>
+            <?php endif; ?>
             
+            <?php if( get_field('update_box_description') ): ?>
+            <p><?php echo get_field('update_box_description')?></p>
+            <?php endif; ?>
             
-             <div class="col-sm">
-                  <?php if(get_field('title')): ?>
-                   <h2><?php echo get_field('title') ?></h2>
-                  <?php endif; ?>
-                  
-                  <?php if( get_field('project_description') ): ?>
-                   <p><?php echo get_field('project_description')?></p>
-                  <?php endif; ?>
-                  
-                  <?php if( get_field('project_image') ): ?>
-                    <img src="<?php echo get_field('project_image')['url']; ?>" class="img-fluid"/>
-                  <?php endif; ?>     
-             </div> 
-            
-          </div>
-       </section>
-     </div>
-
+            <?php if( get_field('update_box_image') ): ?>
+            <img src="<?php echo get_field('update_box_image')['url']; ?>" class="img-fluid"/>
+            <?php endif; ?>     
+        </div> 
+    </div>
+</div>
 <?php endif; ?>
