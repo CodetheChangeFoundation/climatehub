a055a95f203827c2631690c3fc7c92566df68d75
codygagnon <cody.a.gagnon@gmail.com>
2018-11-07 19:03:46 -0800

    CH-6: responsive navbar

--- functions.php
+++ functions.php
@@ -10,3 +10,13 @@ require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/theme-support.php';
 require get_template_directory() . '/inc/title-cf.php';
 require get_template_directory() . '/custom-fields/partners-content-section.php';
+
+function climatehub_theme_setup() {
+  add_theme_support('menus');
+  register_nav_menu('primary', 'Primary Header Navigation');
+}
+
+add_action('init', 'climatehub_theme_setup');
+
+// Include custom navwalker
+require get_template_directory() . '/inc/bs4navwalker.php';
