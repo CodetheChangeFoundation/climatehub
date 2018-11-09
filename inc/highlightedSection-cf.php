30e496ea61ba4c604ede4ce92e69b7f8e1bc1267
Melissa B <melissabernstein.ubc@gmail.com>
2018-11-09 15:27:42 -0800

    CH-27: changed link-to-blog-post from Post Object type to a page link

--- inc/highlightedSection-cf.php
+++ inc/highlightedSection-cf.php
@@ -82,7 +82,7 @@ if( function_exists('acf_add_local_field_group') ):
                 'key' => 'field_5be526cfccfd0',
                 'label' => 'link to blog post',
                 'name' => 'link_to_blog_post',
-                'type' => 'post_object',
+                'type' => 'page_link',
                 'instructions' => '',
                 'required' => 0,
                 'conditional_logic' => array(
@@ -99,12 +99,13 @@ if( function_exists('acf_add_local_field_group') ):
                     'class' => '',
                     'id' => '',
                 ),
-                'post_type' => '',
+                'post_type' => array(
+                    0 => 'post',
+                ),
                 'taxonomy' => '',
                 'allow_null' => 0,
+                'allow_archives' => 1,
                 'multiple' => 0,
-                'return_format' => 'object',
-                'ui' => 1,
             ),
         ),
         'location' => array(
