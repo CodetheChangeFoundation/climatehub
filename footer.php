9b66332069436feb4fc7b0cb444b882e32668327
Melissa B <melissabernstein.ubc@gmail.com>
2018-11-08 20:24:56 -0800

    removed *class font-weight-bold* as it is already built into h6 header

--- footer.php
+++ footer.php
@@ -11,27 +11,21 @@
     Creating a vibrant community around bold climate action at UBC.
     </div>
     <div class="col-sm-4 text-center mt-2 mb-2">
-
-      <h6 class="font-weight-bold ">FOLLOW US</h6>
+      <h6>FOLLOW US</h6>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <a href="https://www.facebook.com/UBCClimateHub/" class="fa fa-facebook"></a>
         <a href="#" class="fa fa-twitter"></a>
         <a href="#" class="fa fa-instagram"></a>
-
     </div>
     <div class="col-sm-2 right">
       Help us achieve our vision! We appreciate your support.
     </div>
     <div class="col-sm-2 align-middle mt-2 mb-2">
       <button type="button" class="btn btn-outline-primary font-italic align-middle">donate</button>
-
     </div>
   </div>
-
   <div class="row bg-dark text-white text-center">
-
     <div class="col text-center" >
-
         Copyright &copy; <script type="text/javascript">
           document.write(new Date().getFullYear());
       </script>
