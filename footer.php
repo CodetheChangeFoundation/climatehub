651cc72182445ab31fa76e0d07dcce25e7f32039
Melissa B <melissabernstein.ubc@gmail.com>
2018-11-08 20:15:00 -0800

    renamed classes (css) and deleted excessive lines and classes (footer)

--- footer.php
+++ footer.php
@@ -5,10 +5,9 @@
 <div class="container-fluid text-center">
   <div class="row align-items-center bg-secondary" style="footer">
     <div class="col-sm-2">
-      <img src="<?php echo get_template_directory_uri() . '/assets/images/ClimateHubLogo.gif'; ?>" class="img-fluid border-0 chlogo"/>
+      <img src="<?php echo get_template_directory_uri() . '/assets/images/ClimateHubLogo.gif'; ?>" class="img-fluid border-0 footer-logo"/>
     </div>
     <div class="col-sm-2">
-    <?php get_template_part( 'template-parts/footer_customfield' )?>
     Creating a vibrant community around bold climate action at UBC.
     </div>
     <div class="col-sm-4 text-center mt-2 mb-2">
@@ -16,7 +15,7 @@
       <h6 class="font-weight-bold ">FOLLOW US</h6>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <a href="https://www.facebook.com/UBCClimateHub/" class="fa fa-facebook"></a>
-        <a href="#" class="fa fa-twitter icon"></a>
+        <a href="#" class="fa fa-twitter"></a>
         <a href="#" class="fa fa-instagram"></a>
 
     </div>
@@ -32,13 +31,13 @@
   <div class="row bg-dark text-white text-center">
 
     <div class="col text-center" >
-      <h6> </h6> <!-- vspace for phone usage -->
+
         Copyright &copy; <script type="text/javascript">
           document.write(new Date().getFullYear());
       </script>
       Climate Hub. Created by
       <a href="http://codethechange.ca/"> Code the Change Foundation</a>
-      <h6> </h6> <!-- vspace for phone usage -->
+
     </div>
   </div>
 </div>
