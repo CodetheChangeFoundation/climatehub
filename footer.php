0706c34da5c2e6b4ba0c3adca10878396d2d3f54
Melissa B <melissabernstein.ubc@gmail.com>
2018-11-09 12:22:51 -0800

    CH-1: added instagram link to social media icon

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <a href="https://www.facebook.com/UBCClimateHub/" class="fa fa-facebook"></a>
         <a href="#" class="fa fa-twitter"></a>
-        <a href="#" class="fa fa-instagram"></a>
+        <a href="https://www.instagram.com/ubcclimatehub/?hl=en" class="fa fa-instagram"></a>
     </div>
     <div class="col-sm-2 right">
       Help us achieve our vision! We appreciate your support.
