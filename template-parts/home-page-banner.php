40a76dc996c74f6b17a8dc28104ad9d55a3b26f9
vtotient <quique.sira@gmail.com>
2018-11-14 20:00:09 -0800

    CH-37: Need to fix pin

--- template-parts/home-page-banner.php
+++ template-parts/home-page-banner.php
@@ -3,8 +3,8 @@
 
   <div class="row ml-4 mr-4 pb-4" style="background-image: url('<?php the_field('home_page_image'); ?>')" >
 
-    <div class="col pl-4 md-6 align-self-end">
-      <h4 class="font-weight-bold text-white overlay mt-5"> <?php the_field("home_page_title"); ?> </h4>
+    <div class="col pl-4 md-6">
+      <h4 class="font-weight-bold text-white overlay"> <?php the_field("home_page_title"); ?> </h4>
       <?php if( get_field("enable_home_page_text") && get_field("home_page_text") ): ?>
         <p class="text-white pt-3"> <?php echo get_field("home_page_text", false, false); ?> </p>
       <?php endif; ?>
