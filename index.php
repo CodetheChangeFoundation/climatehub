36614631ba9f1f729c7e54460a9ef21a128d3043
codygagnon <cody.a.gagnon@gmail.com>
2018-11-17 14:24:13 -0800

    CH-26 added about us title paragraph custom field

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
   <h1>This is my index.</h1>
 
   <div class="container">
-    <?php get_template_part( "template-parts/title" ); ?>
+    <?php get_template_part( 'template-parts/title' ); ?>
   </div>
 
   <div class="container">
-    <?php get_template_part( "template-parts/paragraph") ?>
+    <?php get_template_part( 'template-parts/paragraph') ?>
   </div>
 
   <?php get_template_part( 'template-parts/partners-content-section' )?>
   <?php get_template_part( 'template-parts/updatebox-section' )?>
+  <?php get_template_part( 'template-parts/about-us-title-paragraph' )?>
 <?php get_footer(); ?>
