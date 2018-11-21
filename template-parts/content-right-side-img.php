b2037e115e8bf78469542849d26b612232728596
Melissa B <melissabernstein.ubc@gmail.com>
2018-11-21 12:45:39 -0800

    CH-38: added join us button

--- template-parts/content-right-side-img.php
+++ template-parts/content-right-side-img.php
@@ -4,12 +4,16 @@
         <div class="col">
             <h4><?php the_field('content_w/_right_side_img_title') ?> </h4>
             <p><?php the_field('content_w/_right_side_img_paragraph') ?></p>
+            <button type="button"
+        href="<?php the_field('content_w/_right_side_img_join_us_button'); ?>"
+        class="btn btn-outline-primary font-italic align-middle">join us</button>
 
         </div>
         <div class="col">
-        <?php if( get_field('content_w/_right_side_img_picture') ): ?>
+         <?php if( get_field('content_w/_right_side_img_picture') ): ?>
             <img src="<?php the_field('content_w/_right_side_img_picture'); ?>" class="img-fluid"  />
-        <?php endif; ?>
+         <?php endif; ?>
+
 
 
         </div>
