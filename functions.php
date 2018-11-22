41b6dfc4fb042fc9612768d9db38bc1d7738427d
Melissa B <melissabernstein.ubc@gmail.com>
2018-11-21 20:33:24 -0800

    CH-38: merge conflicts in functions

--- functions.php
+++ functions.php
@@ -14,8 +14,6 @@ require get_template_directory() . '/inc/paragraph-cf.php';
 require get_template_directory() . '/inc/highlightedSection-cf.php';
 require get_template_directory() . '/inc/content-right-side-img-cf.php';
 require get_template_directory() . '/custom-fields/partners-content-section.php';
-<<<<<<< HEAD
-=======
 require get_template_directory() . '/inc/bs4navwalker.php';
 require get_template_directory() . '/custom-fields/updatebox-section.php';
 >>>>>>> 06e3a5cd8ee5a8028a5447a304e52de6afa1cafe
