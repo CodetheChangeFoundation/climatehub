bc90ac736724cb8e1fb90919e658eac02139b938
Melissa B <melissabernstein.ubc@gmail.com>
2018-11-21 19:00:57 -0800

    CH-38: formatted button to hug bottom left, positioned picture

--- template-parts/content-right-side-img.php
+++ template-parts/content-right-side-img.php
@@ -1,21 +1,17 @@
 <?php if(get_field('enable_content_w/_right_side_img_cf')): ?>
 <div class="container-fluid">
-    <div class="row">
-        <div class="col">
+    <div class="row bg-secondary">
+        <div class="col mt-4 mb-4 mr-4 ">
             <h4><?php the_field('content_w/_right_side_img_title') ?> </h4>
             <p><?php the_field('content_w/_right_side_img_paragraph') ?></p>
             <button type="button"
         href="<?php the_field('content_w/_right_side_img_join_us_button'); ?>"
         class="btn btn-outline-primary font-italic align-middle">join us</button>
-
         </div>
         <div class="col">
          <?php if( get_field('content_w/_right_side_img_picture') ): ?>
-            <img src="<?php the_field('content_w/_right_side_img_picture'); ?>" class="img-fluid"  />
+            <div class="w-100 h-100 content-right-side-background-image" style="background-image: url('<?php the_field('content_w/_right_side_img_picture'); ?>')"></div>
          <?php endif; ?>
-
-
-
         </div>
     </div>
 </div>
