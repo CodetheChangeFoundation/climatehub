fe5f2d3aff04401f60599057e6c101562effac3e
Melissa B <melissabernstein.ubc@gmail.com>
2018-11-21 19:53:53 -0800

    CH-38: formatted php with variables, changed button text

--- inc/content-right-side-img-cf.php
+++ inc/content-right-side-img-cf.php
@@ -1,154 +1,149 @@
 <?php
 if( function_exists('acf_add_local_field_group') ):
 
-	acf_add_local_field_group(array(
-		'key' => 'group_5be5f0298d419',
-		'title' => 'Content with right side image CF',
-		'fields' => array(
-			array(
-				'key' => 'field_5be5f03f71484',
-				'label' => 'enable cf',
-				'name' => 'enable_content_w/_right_side_img_cf',
-				'type' => 'true_false',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'message' => '',
-				'default_value' => 0,
-				'ui' => 0,
-				'ui_on_text' => '',
-				'ui_off_text' => '',
+acf_add_local_field_group(array(
+	'key' => 'group_5be5f0298d419',
+	'title' => 'Content with right side image CF',
+	'fields' => array(
+		array(
+			'key' => 'field_5be5f03f71484',
+			'label' => 'enable cf',
+			'name' => 'enable_content_w/_right_side_img_cf',
+			'type' => 'true_false',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
 			),
-			array(
-				'key' => 'field_5be5f05371485',
-				'label' => 'Title',
-				'name' => 'content_w/_right_side_img_title',
-				'type' => 'text',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => array(
-					array(
-						array(
-							'field' => 'field_5be5f03f71484',
-							'operator' => '==',
-							'value' => '1',
-						),
-					),
-				),
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'default_value' => 'Default',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'maxlength' => '',
-			),
-			array(
-				'key' => 'field_5be5f06d71486',
-				'label' => 'Paragraph',
-				'name' => 'content_w/_right_side_img_paragraph',
-				'type' => 'textarea',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => array(
-					array(
-						array(
-							'field' => 'field_5be5f03f71484',
-							'operator' => '==',
-							'value' => '1',
-						),
-					),
-				),
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'default_value' => '',
-				'placeholder' => '',
-				'maxlength' => '',
-				'rows' => '',
-				'new_lines' => '',
-			),
-			array(
-				'key' => 'field_5be5f0be2f1c0',
-				'label' => 'Picture',
-				'name' => 'content_w/_right_side_img_picture',
-				'type' => 'image',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => array(
-					array(
-						array(
-							'field' => 'field_5be5f03f71484',
-							'operator' => '==',
-							'value' => '1',
-						),
-					),
-				),
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'return_format' => 'url',
-				'preview_size' => 'thumbnail',
-				'library' => 'all',
-				'min_width' => '',
-				'min_height' => '',
-				'min_size' => '',
-				'max_width' => '',
-				'max_height' => '',
-				'max_size' => '',
-				'mime_types' => '',
-			),
-			array(
-				'key' => 'field_5be5f157fb05b',
-				'label' => 'Join Us Button',
-				'name' => 'content_w/_right_side_img_join_us_button',
-				'type' => 'page_link',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'post_type' => '',
-				'taxonomy' => '',
-				'allow_null' => 0,
-				'allow_archives' => 1,
-				'multiple' => 0,
-			),
+			'message' => '',
+			'default_value' => 0,
+			'ui' => 0,
+			'ui_on_text' => '',
+			'ui_off_text' => '',
 		),
-		'location' => array(
-			array(
+		array(
+			'key' => 'field_5be5f05371485',
+			'label' => 'Title',
+			'name' => 'content_w/_right_side_img_title',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => array(
 				array(
-					'param' => 'page_template',
-					'operator' => '==',
-					'value' => 'default',
+					array(
+						'field' => 'field_5be5f03f71484',
+						'operator' => '==',
+						'value' => '1',
+					),
 				),
 			),
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => 'Default',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
 		),
-		'menu_order' => 0,
-		'position' => 'normal',
-		'style' => 'default',
-		'label_placement' => 'top',
-		'instruction_placement' => 'label',
-		'hide_on_screen' => '',
-		'active' => 1,
-		'description' => '',
-	));
+		array(
+			'key' => 'field_5be5f06d71486',
+			'label' => 'Paragraph',
+			'name' => 'content_w/_right_side_img_paragraph',
+			'type' => 'textarea',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => array(
+				array(
+					array(
+						'field' => 'field_5be5f03f71484',
+						'operator' => '==',
+						'value' => '1',
+					),
+				),
+			),
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'maxlength' => '',
+			'rows' => '',
+			'new_lines' => '',
+		),
+		array(
+			'key' => 'field_5be5f0be2f1c0',
+			'label' => 'Picture',
+			'name' => 'content_w/_right_side_img_picture',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => array(
+				array(
+					array(
+						'field' => 'field_5be5f03f71484',
+						'operator' => '==',
+						'value' => '1',
+					),
+				),
+			),
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'preview_size' => 'thumbnail',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+		),
+		array(
+			'key' => 'field_5be5f157fb05b',
+			'label' => 'Join Us Button',
+			'name' => 'content_w/_right_side_img_join_us_button',
+			'type' => 'link',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'array',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'page_template',
+				'operator' => '==',
+				'value' => 'default',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
 
-	endif;
-
+endif;
 ?>
--- template-parts/content-right-side-img.php
+++ template-parts/content-right-side-img.php
@@ -1,19 +1,26 @@
-<?php if(get_field('enable_content_w/_right_side_img_cf')): ?>
+<?php
+    $enable = get_field('enable_content_w/_right_side_img_cf');
+    $title = get_field('content_w/_right_side_img_title');
+    $paragraph = get_field('content_w/_right_side_img_paragraph');
+    $link = get_field('content_w/_right_side_img_join_us_button');
+    $picture = get_field('content_w/_right_side_img_picture');
+    if($enable && $title && $paragraph && $link && $picture):
+?>
 <div class="container-fluid">
     <div class="row bg-secondary">
         <div class="col mt-4 mb-4 mr-4 ">
-            <h4><?php the_field('content_w/_right_side_img_title') ?> </h4>
-            <p><?php the_field('content_w/_right_side_img_paragraph') ?></p>
-            <button type="button"
-        href="<?php the_field('content_w/_right_side_img_join_us_button'); ?>"
-        class="btn btn-outline-primary font-italic align-middle">join us</button>
+            <h4><?php echo $title ?> </h4>
+            <p><?php echo $paragraph ?></p>
+            <a type="button"
+              href="<?php echo $link['url']; ?>"
+              class="btn btn-outline-primary font-italic align-middle"
+              target="<?php echo $link['target']; ?>">
+            <?php echo $link['title']; ?>
+            </a>
         </div>
         <div class="col">
-         <?php if( get_field('content_w/_right_side_img_picture') ): ?>
             <div class="w-100 h-100 content-right-side-background-image" style="background-image: url('<?php the_field('content_w/_right_side_img_picture'); ?>')"></div>
-         <?php endif; ?>
         </div>
     </div>
 </div>
-
-<?php endif ?>
+<?php endif; ?>
