2b05ca3e5a0858782bbeff9cf6bdc6234d0d75c6
vtotient <quique.sira@gmail.com>
2019-01-16 18:08:39 -0800

    CH-46: All done expect for center

--- index.php
+++ index.php
@@ -25,4 +25,6 @@
   <?php get_template_part( 'template-parts/partners-content-section' )?>
   <?php get_template_part( 'template-parts/updatebox-section' )?>
 
+  <?php get_template_part( 'template-parts/blog-call-to-action-box') ?>
+
 <?php get_footer(); ?>
