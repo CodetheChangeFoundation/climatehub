e9b01911f289cd9c7ee3b931192a52fe1c2ddbfb
melissab1238 <melissabernstein.ubc@gmail.com>
2019-01-28 21:49:09 -0800

    CH-35: sliding carousel, still not displaying 3 cards at once

--- template-parts/projects-page-carousel.php
+++ template-parts/projects-page-carousel.php
@@ -19,19 +19,39 @@
   </ul>
 
   <!-- The slideshow -->
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-        <div class = "container row">
-        <?php for($i = 0; $i<2; $i++){
+  <div class="carousel-inner row">
+    <div class="carousel-item col-md-4 active">
+        <?php
+        the_row();
+        $current_row = $items[0];
+        $title = $current_row['project_title'];
+        $description = $current_row['project_description'];
+        $link = $current_row['project_link'];
+        $image = $current_row['project_image'];
+        if( $title && $description && $link && $image): ?>
+                <a href="<?php echo $link; ?>" class="text-muted no-text-decoration">
+                <div class="card">
+                    <div class="card-body bg-secondary">
+                        <h5 class="card-title"><?php echo $title; ?> </h5>
+                        <p class="card-text"><?php echo $description; ?></p>
+                    </div>
+                    <img class="card-img-bottom img-carousel" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+                </div>
+                </a>
+
+        <?php endif; ?>
+    </div>
+
+        <?php for($i = 1; $i<$numrows; $i++){
+
             the_row();
             $current_row = $items[$i];
             $title = $current_row['project_title'];
-		    $description = $current_row['project_description'];
-		    $link = $current_row['project_link'];
+		        $description = $current_row['project_description'];
+		        $link = $current_row['project_link'];
             $image = $current_row['project_image'];
-
             if( $title && $description && $link && $image): ?>
-    		    <div class="col-sm-4">
+            <div class="carousel-item col-md-4">
                     <a href="<?php echo $link; ?>" class="text-muted no-text-decoration">
                     <div class="card">
                         <div class="card-body bg-secondary">
@@ -41,52 +61,19 @@
                         <img class="card-img-bottom img-carousel" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
                     </div>
                     </a>
-                </div>
-            <?php  endif; } ?>
-        </div>
-    </div>
-    <div class="carousel-item">
-      <div class = "container row">
-        <?php $max = 3; for($i = 0; $i<$max; $i++){ ?>
-        <div class="col-sm-4">
-          <a href="#" class="text-muted no-text-decoration">
-          <div class="card">
-            <div class="card-body bg-secondary">
-              <h5 class="card-title"><?php echo $title1; ?> </h5>
-              <p class="card-text"><?php echo $description1; ?></p>
             </div>
-          <img class="card-img-bottom img-carousel" src="<?php echo get_template_directory_uri(); ?>/assets/images/ClimateHubLogo.png" alt="Card image cap">
-          </div>
-          </a>
-        </div>
-        <?php } ?>
-      </div>
+            <?php endif; } ?>
+
     </div>
-    <div class="carousel-item">
-      <div class="containter row">
-        <?php $max = 3; for($i = 0; $i<$max; $i++){ ?>
-        <div class="col-sm-4">
-          <a href="#" class="text-muted no-text-decoration">
-          <div class="card">
-            <div class="card-body bg-secondary">
-              <h5 class="card-title"><?php echo $title1; ?> </h5>
-              <p class="card-text"><?php echo $description1; ?></p>
-            </div>
-            <img class="card-img-bottom img-carousel" src="<?php echo get_template_directory_uri(); ?>/assets/images/ClimateHubLogo.png" alt="Card image cap">
-          </div>
-          </a>
-        </div>
-        <?php } ?>
-      </div>
-    </div>
-  </div>
 
   <!-- Left and right controls -->
   <a class="carousel-control-prev" href="#demo" data-slide="prev">
-    <!-- <span class="carousel-control-prev-icon"></span> -->
+    <span class="carousel-control-prev-icon"></span>
+    <span class="sr-only">Previous</span>
   </a>
   <a class="carousel-control-next" href="#demo" data-slide="next">
-    <!-- <span class="carousel-control-next-icon"></span> -->
+    <span class="carousel-control-next-icon"></span>
+    <span class="sr-only">Next</span>
   </a>
 
 </div>
