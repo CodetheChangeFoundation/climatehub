4c80a00b62f45d3b480a163ec0f1fa4405df3714
Viniel Kumar <vinielk@icloud.com>
2019-02-03 12:27:53 -0800

    CH-53: 1. fixed bootstrap custom-select 2. created table for displaying data

--- assetmap/src/App.tsx
+++ assetmap/src/App.tsx
@@ -1,4 +1,4 @@
-// import * as d3 from 'd3';
+import * as d3 from 'd3';
 import * as React from 'react';
 import '../../assets/css/bootstrap/bootstrap-grid.min.css';
 import '../../assets/css/bootstrap/bootstrap-reboot.min.css';
@@ -6,14 +6,42 @@ import '../../assets/css/bootstrap/bootstrap.css';
 import '../../assets/css/climatehub.css';
 import SearchForm from './components/SearchForm';
 
-class App extends React.Component {
+interface MyState {
+  error: any,
+  isLoaded: boolean,
+  items: any
+};
+
+class Assetmap extends React.Component<{}, MyState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [["1", "2", "3"],["1", "2", "3"],["1", "2", "3"]]
+    };
+  }
+  
+  componentDidMount() {
+    // Make API Call
+    // Change state based on response
+    this.setState({isLoaded: true});
+  }
+
   public render() {
-    return (
-      <div className="App">
-        <SearchForm categories={["Communities", "Groups", "Individuals", "Projects"]} columns={["1", "2", "3"]} data={[["a", "b", "c"], ["a", "b", "c"], ["a", "b", "c"]]}/>
-      </div>
-    );
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="Assetmap">
+          <SearchForm categories={["Communities", "Groups", "Individuals", "Projects"]} columns={["1", "2", "3"]} data={items}/>
+        </div>
+      );
+    }
   }
 }
 
-export default App;
+export default Assetmap;
--- assetmap/src/components/SearchForm.tsx
+++ assetmap/src/components/SearchForm.tsx
@@ -36,22 +36,22 @@ class SearchForm extends React.Component<MyProps, MyState> {
     const table = [];
 
     for (const category of this.props.categories) {
-      dropdown.push(<option key={category}>{category}</option>);
+      dropdown.push(<option key={this.props.categories.indexOf(category)}>{category}</option>);
     }
 
     for (const column of this.props.columns) {
-      tableColumns.push(<th className="border-top-0" key={column} scope="col">{column}</th>);
+      tableColumns.push(<th className="border-top-0" key={this.props.columns.indexOf(column)} scope="col">{column}</th>);
     }
 
     for (const row of this.props.data) {
       const cells = []
       for (const cell of row) {
-        cells.push(<td key={cell}>{cell}</td>);
+        cells.push(<td key={row.indexOf(cell)}>{cell}</td>);
       }
-      tableRows.push(<tr>{cells}</tr>);
+      tableRows.push(<tr key={this.props.data.indexOf(row)}>{cells}</tr>);
     }
 
-    table.push(<table className="mb-0 table text-center"><thead>{tableColumns}</thead><tbody>{tableRows}</tbody></table>);
+    table.push(<table key="0" className="mb-0 table text-center"><thead><tr>{tableColumns}</tr></thead><tbody>{tableRows}</tbody></table>);
 
     return (
       <form id="SearchForm" className="container mt-5 pt-5">
@@ -62,12 +62,12 @@ class SearchForm extends React.Component<MyProps, MyState> {
                 <path clipRule="evenodd" fill="#888888" fillRule="evenodd" d="M11.1189 6.55945C11.1189 9.07756 9.07756 11.1189 6.55945 11.1189C4.04133 11.1189 2 9.07756 2 6.55945C2 4.04133 4.04133 2 6.55945 2C9.07756 2 11.1189 4.04133 11.1189 6.55945ZM8.62627 12.7866C7.97647 13.0022 7.28159 13.1189 6.55945 13.1189C2.93676 13.1189 0 10.1821 0 6.55945C0 2.93676 2.93676 0 6.55945 0C10.1821 0 13.1189 2.93676 13.1189 6.55945C13.1189 8.74448 12.0505 10.68 10.4076 11.8721L14.523 19L12.7909 20L8.62627 12.7866Z"/>
               </svg>
             </span>
-            <select className="bg-light border-0 custom-select pl-5 py-1 my-2" onChange={this.handleFilter} value={this.state.filterParameter}>
+            <select className="bg-light border-0 custom-select my-2 pl-5 py-1 shadow-none" onChange={this.handleFilter} value={this.state.filterParameter}>
               {dropdown}
             </select>
           </div>
           <div className="border border-bottom-0 border-dark border-left-0 col-12 col-sm-9 form-group m-0 p-0">
-            <input className="border-0 bg-light form-control py-1 my-2" onChange={this.handleSearch} placeholder="Search" type="text" value={this.state.searchTerm}/>
+            <input className="border-0 bg-light form-control my-2 py-1" onChange={this.handleSearch} placeholder="Search" type="text" value={this.state.searchTerm}/>
           </div>
           <div className="border border-dark col-12">
             <div className="mx-4 overflow-hidden">
