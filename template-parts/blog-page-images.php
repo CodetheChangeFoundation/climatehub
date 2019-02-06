f1a1e052814e8bbf51343a5afac9733bd36111d4
vtotient <quique.sira@gmail.com>
2019-02-06 15:53:09 -0800

    CH-48: Images display. Working on text

--- template-parts/blog-page-images.php
+++ template-parts/blog-page-images.php
@@ -8,42 +8,71 @@
  * The user should select an option in WordPress. See wikipage.
  */
 
-$sel       = get_field("blog_page_select_image_size");
-$img_full  = get_field("blog_page_full_size");
-$img_med   = get_field("blog_page_paragraph_size");
+$sel         = get_field("blog_page_select_image_size");
+$img_full    = get_field("blog_page_full_size");
+$img_med     = get_field("blog_page_paragraph_size");
 $img_small_r = get_field("blog_page_small_right");
 $img_small_l = get_field("blog_page_small_left");
+$img_txt     = get_field("blog_page_image_text");
 
 $full    = "Full page width";
 $med     = "Paragraph width";
 $right   = "Small right";
 $left    = "Small left";
 
-$size_full = "full";
-$size_med  = "medium";
-$size_small ="small";
+$size_full  = "full";
+$size_med   = "full";
+$size_small = "full";
+?>
 
-  switch ($sel) {
-    case $full :
-      if( $img_full ){
-        echo wp_get_attachment_image( $img_full, $size_full );
-      }
+<div class="container">
+  <div class="row justify-content-center"> <?php
+    switch ($sel) {
+      case $full :
+        if( $img_full ){
+          echo wp_get_attachment_image( $img_full, $size_full );
+          ?></div><?php
+          ?><div class="row pl-3"><?php
+          text_display_bp($img_txt);
+        }
 
-    case $med :
-      if( $img_med ){
-        echo wp_get_attachment_image( $img_med, $size_med );
-      }
+      case $med :
+        if( $img_med ){
+            echo wp_get_attachment_image( $img_med, $size_med );
+            ?></div><?php
+            ?><div class="row pl-3 justify-content-left"><?php
+            text_display_bp($img_txt);
+        }
 
-    case $right :
-      if( $img_small_r ){
-        echo wp_get_attachment_image( $img_small_r, $size_small );
-      }
+      case $right :
+        if( $img_small_r ){
+          echo wp_get_attachment_image( $img_small_r, $size_small );
+          ?></div><?php
+          ?><div class="row pl-3"><?php
+          text_display_bp($img_txt);
+        }
 
-    case $left :
-      if( $img_small_l ){
-        echo wp_get_attachment_image( $img_small_l, $size_small );
-      }
+      case $left :
+        if( $img_small_l ){
+          echo wp_get_attachment_image( $img_small_l, $size_small );
+          ?></div><?php
+          ?><div class="row pl-3"><?php
+          text_display_bp($img_txt);
+        }
 
-    default :
-      break;
+      default :
+        break;
+    }
+  ?>
+  </div>
+</div>
+
+<?php
+/* If there is image caption display bottom left */
+function text_display_bp($txt) {
+  if( $txt ){
+    ?> <p class="text-left"> <?php echo $txt; ?> </p> <?php
   }
+  return;
+}
+?>
