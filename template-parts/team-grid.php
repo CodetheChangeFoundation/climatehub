7e66b6d2e67344e9ff418a67bda8be505ff9fd38
melissab1238 <melissabernstein.ubc@gmail.com>
2019-03-13 20:12:24 -0700

    CH-25: redid forloop, better formatting, added title/description

--- template-parts/team-grid.php
+++ template-parts/team-grid.php
@@ -1,64 +1,33 @@
 <?php
 $enable = get_field('team_grid_enable');
+$title = get_field('team_grid_title');
+$description_paragraph = get_field('team_grid_description');
 if($enable):
     $members = get_field('team_members');
-    $numitems = 0;
-    $i = 0;
-    while(have_rows('team_members')): the_row();
-        $numitems++;
-    endwhile;
+    $num = sizeof($members);
 endif; ?>
-<div class = "container-fluid">
-    <div class="row">
-        <?php for($i; $i<4; $i++){
-            the_row();
-            $current_member = $members[$i];
-            $image = $current_member['member_image'];
-            $name = $current_member['member_name'];
-            $description = $current_member['member_description'];?>
-            <div class="col text-center">
-                <img class="img-fluid" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
-                <h6 class="font-weight-bold"><?php echo $name; ?></h6>
-                <h7><?php echo $description;?></h7>
-            </div>
-        <?php } ?>
-    </div>
-    <div class="row">
-        <?php for($i; $i<4; $i++){
-            the_row();
-            $current_member = $members[$i];
-            $image = $current_member['member_image'];
-            $name = $current_member['member_name'];
-            $description = $current_member['member_description'];?>
-            <div class="col  text-center">
-                <img class="img-fluid" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
-                <h5 class="font-weight-bold"><?php echo $name; ?></h5>
-                <h5><?php echo $description;?></h5>
-            </div>
 
-        <?php } ?>
-    </div>
-    <div class="row center-block">
-        <?php for($i; $i<4; $i++){
-            the_row();
-            $current_member = $members[$i];
-            $image = $current_member['member_image'];
-            $name = $current_member['member_name'];
-            $description = $current_member['member_description'];?>
-            <div class="col  text-center">
-                <img class="img-fluid" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
-                <h5 class="font-weight-bold"><?php echo $name; ?></h5>
-                <h5><?php echo $description;?></h5>
-            </div>
+<h1 class="p-2"><?php echo $title; ?> </h1>
+<h5 class="p-2"><?php echo $description_paragraph; ?></h5>
 
-        <?php } ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <?php
+            for($i=0; $i<$num; $i++){
+                the_row();
+                $current_member = $members[$i];
+                $image = $current_member['member_image'];
+                $name = $current_member['member_name'];
+                $description = $current_member['member_description'];?>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 text-center">
+                    <img class="img-fluid" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+                    <?php if($name || $description): ?>
+                    <div class="bg-white p-2 mb-3">
+                        <h5 class="text-left font-weight-bold"><?php echo $name; ?></h5>
+                        <h5 class="text-left m-0"><?php echo $description;?></h5>
+                    </div>
+                    <?php endif; ?>
+                </div>
+            <?php } ?>
     </div>
-
 </div>
-
-<!-- do a for loop for every repeater field (with subgroups image, title, opti description) and insert col
-        after 4 (when i = 4), make new row , and continue inserting col
-        there might be an issue with centering -> can figure out later
-        todo: ask liam if there will always be 8 board of directors? or a certain number of staff? volunteers? -->
-
-<!-- https://www.codeply.com/go/MJTglTsq9h -->
