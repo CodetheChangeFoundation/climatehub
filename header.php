6ef6fe4b6f37d5e9e92c21a4e85f216ca999b6cc
Viniel Kumar <vinielk@icloud.com>
2019-03-26 22:12:35 -0700

    refactor: organized template-parts folder + minor changes to styling

--- header.php
+++ header.php
@@ -1,34 +1,41 @@
 <!doctype html>
-<html>
+<html lang="en">
   <head>
-    <meta charset="utf-8">
-    <title>Climate Hub</title>
-    <?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title><?php bloginfo( 'name' ); wp_title(); ?></title>
+    <meta name="description" content="<?php bloginfo('description') ?>">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+
+    <?php if (is_singular() && pings_open(get_queried_object())): ?>
+      <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <?php endif; ?>
+    <?php wp_head() ?>
   </head>
   <body>
     <nav class="navbar sticky-top navbar-expand-md navbar-light bg-white text-uppercase font-weight-bold p-0 align-items-stretch">
-      <a class="navbar-brand" href="#"></a>
-
-      <div class="container-fluid">
-        <img class="navbar-brand" src="<?php echo get_template_directory_uri(); ?>/assets/images/ClimateHubLogo.png" alt="logo" width=auto height=130/>
+      <div class="container">
+        <a class="navbar-brand p-0 m-0" href="<?php echo get_home_url() ?>">
+          <img class="navbar-brand" src="<?php echo get_template_directory_uri(); ?>/assets/images/ClimateHubLogo.png" alt="logo" width=auto height=130/>
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+        <?php
+          wp_nav_menu([
+            'menu'            => 'primary',
+            'theme_location'  => 'primary',
+            'container'       => 'div',
+            'container_id'    => 'bs4navbar',
+            'container_class' => 'collapse navbar-collapse h-100',
+            'menu_id'         => false,
+            'menu_class'      => 'navbar-nav ml-auto',
+            'depth'           => 2,
+            'fallback_cb'     => 'bs4navwalker::fallback',
+            'walker'          => new bs4navwalker()
+          ]);
+        ?>
       </div>
-
-      <?php
-      wp_nav_menu([
-      'menu'            => 'primary',
-      'theme_location'  => 'primary',
-      'container'       => 'div',
-      'container_id'    => 'bs4navbar',
-      'container_class' => 'collapse navbar-collapse',
-      'menu_id'         => false,
-      'menu_class'      => 'navbar-nav mr-auto h-100',
-      'depth'           => 2,
-      'fallback_cb'     => 'bs4navwalker::fallback',
-      'walker'          => new bs4navwalker()
-      ]);
-      ?>
     </nav>
 
--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <?php get_header(); ?>
-
+  <!-- Global -->
   <div class="container">
-    <?php get_template_part( 'template-parts/title' ); ?>
+    <?php get_template_part( 'template-parts/global/title' ); ?>
+    <?php get_template_part( 'template-parts/global/paragraph') ?>
   </div>
 
+  <!-- Home Page -->
+  <?php get_template_part("template-parts/home/home-page-banner") ?>
+  <?php get_template_part( 'template-parts/home/home-asset-map' ) ?>
+
   <div class="container">
-    <?php get_template_part( 'template-parts/paragraph') ?>
+    <?php get_template_part("template-parts/home/content-right-side-img") ?>
   </div>
 
-  <div class="container-fluid">
+  <?php get_template_part( 'template-parts/home/updatebox-section' )?>
+
+  <!-- About Page -->
+  <?php get_template_part( 'template-parts/about/about-us-title-paragraph' )?>
+
+  <!-- Partners Page -->
+  <?php get_template_part( 'template-parts/partners/partners-content-section' )?>
+
+  <!-- Contact Page -->
+  <div class="container">
     <?php get_template_part( "template-parts/contact-us" ) ?>
     <?php get_template_part( "template-parts/address-title" ) ?>
   </div>
@@ -17,15 +31,4 @@
     <?php get_template_part( "template-parts/contact-us-button" ) ?>
   </div>
 
-  </div class="jumbotron d-flex flex-column border border-primary justify-content-end align-content-start p-0">
-    <?php get_template_part("template-parts/home-page-banner") ?>
-  </div>
-  <?php get_template_part( 'template-parts/home-asset-map' ) ?>
-
-  <div class="container">
-    <?php get_template_part("template-parts/content-right-side-img") ?>
-  </div>
-  <?php get_template_part( 'template-parts/partners-content-section' )?>
-  <?php get_template_part( 'template-parts/updatebox-section' )?>
-  <?php get_template_part( 'template-parts/about-us-title-paragraph' )?>
 <?php get_footer(); ?>
