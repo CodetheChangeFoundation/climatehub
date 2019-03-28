f16d273db28e83283e046f22de01ae1a37d5be34
Viniel Kumar <vinielk@icloud.com>
2019-03-27 22:21:31 -0700

    refactor: cleanup js file

--- assets/js/climatehub.js
+++ assets/js/climatehub.js
@@ -1,15 +1,13 @@
 // javascript functions
 
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () { scrollFunction(); };
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("logo").style.height = "4rem";
     document.getElementById("logo").style.padding = "0.2rem 0";
-    // $("#bs4navbar ul li").attr("style", "top:-14.5px;");
   } else {
     document.getElementById("logo").style.height = "8rem";
     document.getElementById("logo").style.padding = "1rem 0";
-    // $("#bs4navbar ul li").attr("style", "top:-46.5px;");
   }
 }
