f1484dba3237af0ad773c366c4d97d5c03ef4ba4
Viniel Kumar <vinielk@icloud.com>
2019-05-14 19:09:34 -0700

    clean up projects page files

--- index.php
+++ index.php
@@ -29,40 +29,14 @@
   <?php get_template_part( 'template-parts/partners/partners-grid' )?>
   <?php get_template_part( 'template-parts/partners/partners-content-section' )?>
 
+  <!-- Projects Page -->
+  <?php get_template_part('template-parts/projects/projects-carousel')?>
+  <?php get_template_part('template-parts/projects/featured-projects-carousel')?>
+  <?php get_template_part('template-parts/projects/events-carousel')?>
+
   <!-- Contact Page -->
   <?php get_template_part( "template-parts/contact-us" ) ?>
   <?php get_template_part( "template-parts/address-title" ) ?>
   <?php get_template_part( "template-parts/contact-us-button" ) ?>
-
-  <div class="container pt-4 pb-4">
-    <div id="toggle-past-current" class="text-center">
-      <h2>
-        <a href="?link=current"> current</a>
-        <a href="?link=past">past</a>
-      </h2>
-      <?php $link=$_GET['link'];
-      if($link == 'past'){
-        get_template_part( 'template-parts/projects-page-carousel-past' );
-      } else{
-        get_template_part( 'template-parts/projects-page-carousel-current' );
-      } ?>
-    </div>
-    <!-- todo! enqueue both current and past carousels before clicking hyperlink -->
-    <!-- todo! remove project_carousel_currentpast_toggle -->
-    <!-- todo! add image restriction for update carousel DONE -->
-    <!-- todo! make it not a carousel, hardcode it? -->
-
-  </div>
-
-  <div class="container pt-4 pb-4">
-    <h2 class="text-center" style="text-decoration: underline;"> featured projects from the community </h2>
-    <?php get_template_part( 'template-parts/projects-page-carousel-featured' )?>
-  </div>
-
-  <div class="container pt-4 pb-4">
-    <!-- todo: add style below to css -->
-    <h2 class="text-center" style="text-decoration: underline;"> upcoming events in the community </h2>
-    <?php get_template_part( 'template-parts/projects-page-carousel-upcoming' )?>
-  </div>
 
 <?php get_footer(); ?>
---
+++ template-parts/projects/projects-carousel.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Projects carousel
+ *
+ * @package climatehub
+ */
+?>
+
+
+<nav>
+  <div class="nav" id="nav-tab" role="tablist">
+    <a class="nav-item nav-link active" id="nav-current-tab" data-toggle="tab" href="#nav-current" role="tab" aria-controls="nav-current" aria-selected="true">current</a>
+    <a class="nav-item nav-link" id="nav-past-tab" data-toggle="tab" href="#nav-past" role="tab" aria-controls="nav-past" aria-selected="false">past</a>
+  </div>f
+</nav>
+<div class="tab-content" id="nav-tabContent">
+  <div class="tab-pane fade show active" id="nav-current" role="tabpanel" aria-labelledby="nav-current-tab">1</div>
+  <div class="tab-pane fade" id="nav-past" role="tabpanel" aria-labelledby="nav-past-tab">2</div>
+</div>
