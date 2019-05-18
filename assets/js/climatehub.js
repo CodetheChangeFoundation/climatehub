a48ca28a1fd43e8ddd53312cc0b10c274746d9eb
Viniel Kumar <vinielk@icloud.com>
2019-05-17 23:14:28 -0700

    CH-70: added collapsable content to partners grid

--- assets/js/climatehub.js
+++ assets/js/climatehub.js
@@ -1,5 +1,6 @@
 // javascript functions
 
+// navbar
 $(window).scroll(function () {
   if ($(document).scrollTop() >= 80) {
     $('#logo').addClass('shrink');
@@ -8,72 +9,83 @@ $(window).scroll(function () {
   }
 });
 
+// engagements page
+$('.partner-image').on('click', function() {
+  $('.collapse.show').collapse('toggle');
+});
+
+$('.close-icon').on('click', function() {
+  $('.collapse.show').collapse('toggle');
+});
+
 // google maps
-(function ($) {
-  function render_map($el) {
-    var $markers = $el.find('.marker');
-    var args = {
-      zoom: 16,
-      center: new google.maps.LatLng(0, 0),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+if ($('.acf-map').length) {
+  (function ($) {
+    function render_map($el) {
+      var $markers = $el.find('.marker');
+      var args = {
+        zoom: 16,
+        center: new google.maps.LatLng(0, 0),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
 
-    var map = new google.maps.Map($el[0], args);
-    map.markers = [];
+      var map = new google.maps.Map($el[0], args);
+      map.markers = [];
 
-    $markers.each(function () {
-      add_marker($(this), map);
-    });
-    
-    center_map(map);
-  }
+      $markers.each(function () {
+        add_marker($(this), map);
+      });
+      
+      center_map(map);
+    }
 
-  var infowindow = new google.maps.InfoWindow({
-    content: ''
-  });
-
-  function add_marker($marker, map) {
-    var latlng = new google.maps.LatLng($marker.attr('data-lat'), $marker.attr('data-lng'));
-    var marker = new google.maps.Marker({
-      position: latlng,
-      map: map
+    var infowindow = new google.maps.InfoWindow({
+      content: ''
     });
 
-    map.markers.push(marker);
-
-    if ($marker.html()) {
-      google.maps.event.addListener(marker, 'click', function () {
-        infowindow.setContent($marker.html());
-        infowindow.open(map, marker);
+    function add_marker($marker, map) {
+      var latlng = new google.maps.LatLng($marker.attr('data-lat'), $marker.attr('data-lng'));
+      var marker = new google.maps.Marker({
+        position: latlng,
+        map: map
       });
 
-      google.maps.event.addListener(map, 'click', function (event) {
-        if (infowindow) {
-          infowindow.close();
-        }
+      map.markers.push(marker);
+
+      if ($marker.html()) {
+        google.maps.event.addListener(marker, 'click', function () {
+          infowindow.setContent($marker.html());
+          infowindow.open(map, marker);
+        });
+
+        google.maps.event.addListener(map, 'click', function (event) {
+          if (infowindow) {
+            infowindow.close();
+          }
+        });
+      }
+    }
+
+    function center_map(map) {
+      var bounds = new google.maps.LatLngBounds();
+      
+      $.each(map.markers, function (i, marker) {
+        var latlng = new google.maps.LatLng(marker.position.lat(), marker.position.lng());
+        bounds.extend(latlng);
       });
+
+      if (map.markers.length == 1) {
+        map.setCenter(bounds.getCenter());
+        map.setZoom(16);
+      } else {
+        map.fitBounds(bounds);
+      }
     }
-  }
 
-  function center_map(map) {
-    var bounds = new google.maps.LatLngBounds();
-    
-    $.each(map.markers, function (i, marker) {
-      var latlng = new google.maps.LatLng(marker.position.lat(), marker.position.lng());
-      bounds.extend(latlng);
+    $(document).ready(function () {
+      $('.acf-map').each(function () {
+        render_map($(this));
+      });
     });
-
-    if (map.markers.length == 1) {
-      map.setCenter(bounds.getCenter());
-      map.setZoom(16);
-    } else {
-      map.fitBounds(bounds);
-    }
-  }
-
-  $(document).ready(function () {
-    $('.acf-map').each(function () {
-      render_map($(this));
-    });
-  });
-})(jQuery);
+  })(jQuery);
+}
--- template-parts/partners/partners-grid.php
+++ template-parts/partners/partners-grid.php
@@ -6,32 +6,42 @@
  */
 
 if( get_field('enable_partners_grid') ): ?>
-  <div class="container pt-2">
-    <div class="row">
+  <div class="container">
+    <div class="partner-grid-container">
       <?php 
         if( have_rows('partners_grid') ):
+          $count = 1;
           while ( have_rows('partners_grid') ) : the_row();
             $gridRow = get_sub_field('grid_row');
             $numImages = sizeof($gridRow);
             for($logo=1; $logo<=$numImages; $logo++):
-              $image = $gridRow['image_'.$logo]; ?>
+              $image = $gridRow['image_'.$logo];
+              $partnerId = 'partner-content-'.$count; ?>
               <?php if ($image): ?>
-                <div class="col-12 col-sm-6 col-md-4 col-lg-3 custom-col-xl-2-4 mb-4">
-                  <div class="partner-logo-container m-auto">
-                    <a href="<?php echo $image['url'] ?>">
-                      <div class="partner-logo" style="background-image: url('<?php echo $image['url'] ?>')"></div>
-                    </a>
+                <a class="btn btn-link p-0 border-0" data-toggle="collapse" data-target="#<?php echo $partnerId ?>" aria-expanded="false" aria-controls="<?php echo $partnerId ?>">
+                  <div class="partner-image background-image my-3" style="background-image: url('<?php echo $image['url'] ?>')"></div>
+                </a>
+                <div class="partner-content collapse" id="<?php echo $partnerId ?>">
+                  <div class="container bg-white">
+                    <div class="row">
+                      <a class="close-icon text-primary btn btn-link font-weight-light" role="button">&times;</a>
+                      <div class="col-12 col-md-8 p-4">
+                        <h3><?php echo $image['title'] ?></h3>
+                        <p><?php echo $image['description'] ?></p>
+                      </div>
+                      <div class="col-12 col-md-4 p-4">
+                        <h3>Links</h3>
+                        <a href="<?php echo get_post_meta($image['id'], '_link_one_url', true);?>"><?php echo get_post_meta($image['id'], '_link_one_text', true); ?></a>
+                        <br/>
+                        <a href="<?php echo get_post_meta($image['id'], '_link_two_url', true);?>"><?php echo get_post_meta($image['id'], '_link_two_text', true); ?></a>
+                      </div>
+                    </div>
                   </div>
                 </div>
               <?php else: ?>
-                <div class="d-none d-md-flex col-md-4 col-lg-3 custom-col-xl-2-4 mb-4">
-                  <div class="partner-logo-container m-auto">
-                    <a href="<?php echo $image['url'] ?>">
-                      <div class="partner-logo" style="background-image: url('<?php echo $image['url'] ?>')"></div>
-                    </a>
-                  </div>
-                </div>
+                <div class="partner-image d-none d-md-block"></div>
               <?php endif;
+              $count++;
             endfor;
           endwhile;
         endif;
