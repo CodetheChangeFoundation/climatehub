58b23aaeb3163cd56bb055758290cc8a5aa17227
Viniel Kumar <vinielk@icloud.com>
2019-06-02 16:38:06 -0700

    CH-78: use ACF instead of hard-coding 404 message

--- 404.php
+++ 404.php
@@ -4,19 +4,19 @@
  *
  * @package climatehub
  */
+
+$link = get_field('404_page_button', 'option');
 ?>
 
 <?php get_header(); ?>
-  
+
   <div class="container my-5">
     <div class="row">
-      <div class="col-12 text-center">
-        <div class="h1 font-weight-bold">404</div>
-        <div class="h2">Page Not Found</div>
-        <div class="h3">
-          We can't find the page you're looking for. Go back or head over the the homepage.
+      <div class="col-12">
+        <?php the_field('404_page_content', 'option'); ?>
+        <div class="w-100 text-center mt-4">
+          <a class="btn btn-outline-primary btn-lg" target="<?php echo $link['target']; ?>" href="<?php echo $link['url']; ?>"><?php echo $link['title'] ?></a>
         </div>
-        <a class="btn btn-outline-primary btn-lg mt-4" role="button" href="<?php echo get_home_url(); ?>">Homepage</a>
       </div>
     </div>
   </div>
