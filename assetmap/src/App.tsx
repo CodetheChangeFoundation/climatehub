93ea842b93a23118945f6a43a5b934f9d66e2046
Viniel Kumar <vinielk@icloud.com>
2020-04-11 12:16:50 -0700

    CH-107: make loading gif an svg

--- assetmap/src/App.tsx
+++ assetmap/src/App.tsx
@@ -5,7 +5,6 @@ import '../../assets/css/bootstrap/bootstrap.css';
 import '../../assets/css/climatehub.css';
 import Map from './components/Map';
 import SearchForm from './components/SearchForm';
-import spinner from './images/loading_spinner.gif';
 
 interface MyState {
   cities: any,
@@ -446,7 +445,24 @@ class Assetmap extends React.Component<{}, MyState> {
           <div id="mapParent" className="container">
             <div className="row h-100">
               <div className='col d-flex align-items-center justify-content-center'>
-                <img src={spinner} alt="Loading..." />
+                <svg style={{margin: 'auto', background: 'rgb(242, 242, 242)', display: 'block', shapeRendering: 'auto'}} width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+                  <circle cx="84" cy="50" r="7.00274" fill="#41d3bd">
+                      <animate attributeName="r" repeatCount="indefinite" dur="0.5s" calcMode="spline" keyTimes="0;1" values="10;0" keySplines="0 0.5 0.5 1" begin="0s" />
+                      <animate attributeName="fill" repeatCount="indefinite" dur="2s" calcMode="discrete" keyTimes="0;0.25;0.5;0.75;1" values="#41d3bd;#f2f2f2;#83d335;#f18f01;#41d3bd" begin="0s" />
+                  </circle><circle cx="16" cy="50" r="0" fill="#41d3bd">
+                    <animate attributeName="r" repeatCount="indefinite" dur="2s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1" values="0;0;10;10;10" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="0s" />
+                    <animate attributeName="cx" repeatCount="indefinite" dur="2s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1" values="16;16;16;50;84" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="0s" />
+                  </circle><circle cx="16" cy="50" r="2.99726" fill="#f18f01">
+                    <animate attributeName="r" repeatCount="indefinite" dur="2s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1" values="0;0;10;10;10" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.5s" />
+                    <animate attributeName="cx" repeatCount="indefinite" dur="2s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1" values="16;16;16;50;84" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.5s" />
+                  </circle><circle cx="26.1907" cy="50" r="10" fill="#83d335">
+                    <animate attributeName="r" repeatCount="indefinite" dur="2s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1" values="0;0;10;10;10" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="-1s" />
+                    <animate attributeName="cx" repeatCount="indefinite" dur="2s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1" values="16;16;16;50;84" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="-1s" />
+                  </circle><circle cx="60.1907" cy="50" r="10" fill="#f2f2f2">
+                    <animate attributeName="r" repeatCount="indefinite" dur="2s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1" values="0;0;10;10;10" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="-1.5s" />
+                    <animate attributeName="cx" repeatCount="indefinite" dur="2s" calcMode="spline" keyTimes="0;0.25;0.5;0.75;1" values="16;16;16;50;84" keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1" begin="-1.5s" />
+                  </circle>
+                </svg>
               </div>
             </div>
           </div>
