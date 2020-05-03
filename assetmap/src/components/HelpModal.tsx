275a08f6dc9b9bc0044e441a33f3dd4d8a94b130
sumeetgrewal <sumeetsinghgrewal@gmail.com>
2020-05-02 17:07:10 -0700

    CH-140 added action button

--- assetmap/src/components/HelpModal.tsx
+++ assetmap/src/components/HelpModal.tsx
@@ -41,20 +41,25 @@ export default class HelpModal extends React.Component<{},ModalState> {
     }
     
     public render() {
-        return (
-            <ReactModal 
-                isOpen={this.state.isOpen}
-                style={this.customStyles}
-                closeTimeoutMS={400}
-                shouldCloseOnOverlayClick={true}
-                shouldCloseOnEsc={true}
-                onRequestClose={this.closeModal}
-            >
-                <h1>Welcome to the ClimateHub Asset Map!</h1> 
-                <p>Testing this modal</p>
-                <button style= {{margin: "auto"}} type="button" className="btn btn-outline-primary font-italic" onClick={this.closeModal}>Continue</button>
+        const modalOpen= this.state.isOpen
+        return (<>
+                <ReactModal 
+                    isOpen={modalOpen}
+                    style={this.customStyles}
+                    closeTimeoutMS={400}
+                    shouldCloseOnOverlayClick={true}
+                    shouldCloseOnEsc={true}
+                    onRequestClose={this.closeModal}
+                >
+                    <h1>Welcome to the ClimateHub Asset Map!</h1> 
+                    <p>Testing this modal</p>
+                    <button style= {{margin: "auto"}} type="button" className="btn btn-outline-primary font-italic" onClick={this.closeModal}>Continue</button>
 
-            </ReactModal>
+                </ReactModal>
+                <button onClick={modalOpen? this.closeModal : this.openModal}className="btn btn-outline-primary action-button">
+                    ?
+                </button>
+            </>
         )
     }
 }
